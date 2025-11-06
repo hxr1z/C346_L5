618ad1d369615974a3c7cd4d02b47fa33b7c6c36
hxr1z <06_postfix.feeds@icloud.com>
2025-11-06 11:53:44 +0800

    update

--- App.js
+++ App.js
@@ -1,20 +1,66 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import {View, Text, TextInput, Button, Alert, TouchableOpacity, Image, ToastAndroid} from 'react-native';
+import {Picker} from '@react-native-picker/picker';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+//Define own custom component
+const InputBox = ({label, onChangeText}) => {
+    return (
+        <View>
+            <Text>{label}</Text>
+            <TextInput style={{borderWidth: 1}} onChangeText={onChangeText}/>
+        </View>
+    )
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const MyApp = () => {
+    const [pw, setPw] = useState('');
+    const [name, setName] = useState('');
+    const [type, setType] = useState('');
+
+    return (
+        <View style={{marginTop: 40}}>
+            <Text>User Type:</Text>
+
+
+            <Picker onValueChange={(value) => setType(value)}>
+                <Picker.Item label= 'Admin' value= 'Admin' />
+                <Picker.Item label= 'Guest' value= 'Guest' />
+            </Picker>
+
+            {/*<Text>Username:</Text>*/}
+            {/*<TextInput style={{borderWidth: 1}} onChangeText={(text) => setName(text)} />*/}
+
+            {/*<Text>Password:</Text>*/}
+            {/*<TextInput style={{borderWidth: 1}} onChangeText={(text) => setPw(text)} />*/}
+
+            <InputBox label='Username:' onChangeText={text => setName(text)} />
+            <InputBox label='Password:' onChangeText={text => setPw(text)} />
+
+            {/*<Button title='LOG IN' onPress={() => {*/}
+            {/*    Alert.alert('Welcome!')*/}
+            {/*}} />*/}
+
+            <TouchableOpacity
+                // style={{margin:5, backgroundColor:'skyblue', height:50, justifyContent:'center', alignItems: 'center'}}
+                onPress={() => {
+                // Alert.alert('Welcome!')
+                    const correctPassword = '123';
+                    let mymessage = 'Error! Wrong Password!';
+                    if (pw == correctPassword) {
+                        mymessage = 'Welcome ' + type + ' ' + name;
+                    }
+                    ToastAndroid.show(mymessage, ToastAndroid.SHORT);
+            }}>
+                {/*<Text*/}
+                {/*    >LOG IN</Text>*/}
+                <Image
+                    source={{uri:'https://media.makeameme.org/created/lets-go-click.jpg'}}
+                    style={{width: 200, height: 200,}}
+                    />
+            </TouchableOpacity>
+            <Text>{pw}</Text>
+        </View>
+    );
+};
+
+export default MyApp;
